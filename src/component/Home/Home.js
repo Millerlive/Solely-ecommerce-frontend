c81e311f772acab93149e9ee66ae76cee9e479b6
李苗苗 <12828910+li-miao517@user.noreply.gitee.com>
2023-07-17 18:50:53 +0800

    init

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from "react";
-import { CgMouse } from "react-icons/all";
+// import { CgMouse } from "react-icons/all";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
 import MetaData from "../layout/MetaData";
@@ -9,49 +9,60 @@ import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
 
 const Home = () => {
-  const alert = useAlert();
-  const dispatch = useDispatch();
-  const { loading, error, products } = useSelector((state) => state.products);
+    const alert = useAlert();
+    const dispatch = useDispatch();
+    const { loading, error, products } = useSelector((state) => state.products);
 
-  useEffect(() => {
-    if (error) {
-      alert.error(error);
-      dispatch(clearErrors());
-    }
-    dispatch(getProduct());
-  }, [dispatch, error, alert]);
+    useEffect(() => {
+        if (error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
+        dispatch(getProduct());
+    }, [dispatch, error, alert]);
 
-  return (
-    <Fragment>
-      {loading ? (
-        <Loader />
-      ) : (
-        <Fragment>
-          <MetaData title="ECOMMERCE" />
+    return ( <
+        Fragment > {
+            loading ? ( <
+                Loader / >
+            ) : ( <
+                Fragment >
+                <
+                MetaData title = "ECOMMERCE" / >
 
-          <div className="banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+                <
+                div className = "banner" >
+                <
+                p > Welcome to Ecommerce < /p> <
+                h1 > FIND AMAZING PRODUCTS BELOW < /h1>
 
-            <a href="#container">
-              <button>
-                Scroll <CgMouse />
-              </button>
-            </a>
-          </div>
+                <
+                a href = "#container" >
+                <
+                button > { /* Scroll <CgMouse /> */ } <
+                /button> <
+                /a> <
+                /div>
 
-          <h2 className="homeHeading">Featured Products</h2>
+                <
+                h2 className = "homeHeading" > Featured Products < /h2>
 
-          <div className="container" id="container">
-            {products &&
-              products.map((product) => (
-                <ProductCard key={product._id} product={product} />
-              ))}
-          </div>
-        </Fragment>
-      )}
-    </Fragment>
-  );
+                <
+                div className = "container"
+                id = "container" > {
+                    products &&
+                    products.map((product) => ( <
+                        ProductCard key = { product._id }
+                        product = { product }
+                        />
+                    ))
+                } <
+                /div> <
+                /Fragment>
+            )
+        } <
+        /Fragment>
+    );
 };
 
 export default Home;
